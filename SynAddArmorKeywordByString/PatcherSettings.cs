1a6c20914b939370105f3da42e66ea90f9e198ba
TokcDK <TokcDK@Gmail.com>
2022-08-06 12:09:17 +0500

    added some base settings

---
+++ SynAddArmorKeywordByString/PatcherSettings.cs
@@ -0,0 +1,30 @@
+﻿using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis.Settings;
+using StringCompareSettings;
+
+namespace SynAddArmorKeywordByString
+{
+    [SynthesisObjectNameMember("Keyword data")]
+    public class KeywordData
+    {
+        [SynthesisOrder]
+        [SynthesisDiskName("Keyword")]
+        [SynthesisTooltip("Keyword to set if found by strings")]
+        public FormLink<IKeywordGetter>? Keyword;
+        [SynthesisOrder]
+        [SynthesisDiskName("StringsToSearch")]
+        [SynthesisTooltip("Strings for keyword")]
+        public HashSet<StringCompareSetting>? StringsToSearch;
+    }
+
+    public class PatcherSettings
+    {
+        [SynthesisOrder]
+        [SynthesisDiskName("AddKeywordTemplate")]
+        [SynthesisSettingName("Keyword Template")]
+        [SynthesisTooltip("Strings to search and keyword to set if found")]
+        // public string BaselineMod { get; set; } = "MyFacegenBaseline.esp";
+        public HashSet<KeywordData> AddKeywordTemplate = new();
+    }
+}
